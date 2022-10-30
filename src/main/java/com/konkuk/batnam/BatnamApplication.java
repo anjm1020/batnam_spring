60abda1996565ab266005ba09b4c159cf1c46698
jaemin <anjm1020@gmail.com>
2022-10-30 23:30:41 +0900

    Init : tmp

---
+++ src/main/java/com/konkuk/batnam/BatnamApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.batnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatnamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/batnam/BatnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.batnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
